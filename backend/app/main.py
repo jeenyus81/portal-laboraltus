022e0b9150a2c354577c75a2acd28b99f682fd80
jeenyus81 <davidrolu81@gmail.com>
2026-09-24 10:55:37 +0200

    Corregir CORS del backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -244,13 +244,9 @@ _add_missing_company_user_column()
 
 app.add_middleware(
     CORSMiddleware,
-allow_origins=[
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:5175",
-    "http://localhost:5176",
-    "https://portal-laboraltus-1.onrender.com",
-],
+    allow_origins=[
+        "https://portal-laboraltus-1.onrender.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
